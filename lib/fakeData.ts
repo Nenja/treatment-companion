7994cd891c9e81c616ba49c591a0ce5c8089c016
Nenja <nkarottki@gmail.com>
2026-05-22 00:46:59 +0200

    Delete lib/fakeData.ts

--- lib/fakeData.ts
+++
@@ -1,6 +0,0 @@
-// This file was the prototype's in-memory seed data. It's been deleted
-// from the project. The empty file exists only because GitHub uploads
-// can't delete files; if you have direct repo access, remove this file
-// (and store.ts, components/dev/) entirely.
-
-export {};
